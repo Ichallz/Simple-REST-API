695926d35e1d79deb5799f381d3b658f39cb79ac
Charles Imeh <charlesimeh1136@gmail.com>
2023-09-15 16:27:36 +0100

    updated index

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
- $environment = getenv('ENVIRONMENT');
- echo $environment;
+$isLocalhost = ($_SERVER['REMOTE_ADDR'] === '127.0.0.1' || $_SERVER['REMOTE_ADDR'] === '::1') || ($_SERVER['SERVER_ADDR'] === '127.0.0.1' || $_SERVER['SERVER_ADDR'] === '::1');
+//
+if (!defined('ENVIRONMENT')) define('ENVIRONMENT', $isLocalhost ? 'dev' : 'prod');
+
+echo ENVIRONMENT;
 ?>
